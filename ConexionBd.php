dfdd24c4b72a135a8381352d478c4b25fdc15a24
cdvillamil92 <cristiandavid199@gmail.com>
2017-09-19 23:27:07 -0500

    .gitignore

--- ConexionBd.php
+++ ConexionBd.php
@@ -1,5 +1,5 @@
 <?php
-$bd = new mysqli("localhost", "root", "bczbjsrb", "realTec");
+$bd = new mysqli("localhost", "root", "Chris2992", "realtec");
 // Check connection
 if ($bd->connect_error) {
     die("Connection failed: " . $bd->connect_error);
